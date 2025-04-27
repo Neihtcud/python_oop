d70302b4186e6d04dff2b356e6e49b84c80ad3ec
Neihtcud <tranducthienforwork@gmail.com>
2025-04-28 02:15:08 +0700

    Update helpers.py

--- utils/helpers.py
+++ utils/helpers.py
@@ -1,6 +1,6 @@
 import os
 import time
-
+import re
 def clear_screen():
     os.system('cls' if os.name == 'nt' else 'clear')
 
@@ -11,9 +11,17 @@ def nhap_sdt():
             return sdt
         print("\033[91mSố điện thoại không hợp lệ. Vui lòng nhập lại.\033[0m")
 
-def loading():
-    print("\033[93mĐang xử lý...\033[0m", end="")
+def nhap_ten():
+    while True:
+        ten = input("Tên KH: ").strip()
+        if ten.replace(" ", "").isalpha():
+            return ten
+        print("\033[91mTên không hợp lệ. Tên chỉ được chứa chữ cái và khoảng trắng.\033[0m")
+
+
+def loading(msg="Đang xử lý", dot_count=3, delay=0.4):
+    print(f"\033[93m{msg}\033[0m", end="")
     for _ in range(3):
         print(".", end="", flush=True)
-        time.sleep(0.4)
+        time.sleep(delay)
     print()
