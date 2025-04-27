c0ac4af3eb26bd31ea6ee4d6acdec53ae7df1cd9
Neihtcud <tranducthienforwork@gmail.com>
2025-04-28 02:14:41 +0700

    Update customer.py

--- models/customer.py
+++ models/customer.py
@@ -4,6 +4,9 @@ class Customer:
         self.ten_khach_hang = ten_khach_hang
         self.so_dien_thoai = so_dien_thoai
         self.email = email
+    def __str__(self):
+        return f"{self.ma_khach_hang} - {self.ten_khach_hang}"
+    
 
 class LoyalCustomer(Customer):
     def __init__(self, ma_khach_hang, ten_khach_hang, so_dien_thoai, email):
@@ -14,3 +17,6 @@ class CasualCustomer(Customer):
         super().__init__(ma_khach_hang, ten_khach_hang, so_dien_thoai, email)
         self.so_lan_mua_hang = so_lan_mua_hang
         self.tong_gia_tri_mua_hang = tong_gia_tri_mua_hang
+
+    def __str__(self):
+        return f"{super().__str__()} | Số lần mua: {self.so_lan_mua_hang}, Tổng giá trị: {self.tong_gia_tri_mua_hang}"    
