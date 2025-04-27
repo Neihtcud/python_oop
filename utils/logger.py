c5d2f346826ea7eafda145819d026d4d05c86ba2
Neihtcud <tranducthienforwork@gmail.com>
2025-04-28 02:15:32 +0700

    Update logger.py

--- utils/logger.py
+++ utils/logger.py
@@ -2,7 +2,10 @@ from datetime import datetime
 from models.customer import LoyalCustomer
 
 def ghi_log(hanh_dong, khach_hang):
-    with open('log.txt', 'a', encoding='utf-8') as f:
-        thoi_gian = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-        loai = "Loyal" if isinstance(khach_hang, LoyalCustomer) else "Casual"
-        f.write(f"[{thoi_gian}] {hanh_dong} khách hàng: {khach_hang.ma_khach_hang} - {khach_hang.ten_khach_hang} - {loai}\n")
+    try: 
+        with open('log.txt', 'a', encoding='utf-8') as f:
+            thoi_gian = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+            loai = "Loyal" if isinstance(khach_hang, LoyalCustomer) else "Casual"
+            f.write(f"[{thoi_gian}] {hanh_dong} khách hàng: {khach_hang.ma_khach_hang} - {khach_hang.ten_khach_hang} - {loai}\n")
+    except Exception as e:
+        print(f"\033[91mLỗi ghi log: {e}\033[0m")
